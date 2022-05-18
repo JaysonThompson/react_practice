c65887e38207eaee366fd97d38971fd52d1cce03
Jayson Thompson <jaysonthompson922@gmail.com>
2022-05-17 23:47:29 -0700

    Added my project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import MainContent from './components/MainContent'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <MainContent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <>
+      <h2>Main Content Section</h2>
+      <p>This is the main content sections paragraph</p>
+    </>
+  )
+}
+
+export default MainContent
